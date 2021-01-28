495694d50fbbccd85c563b5cde3375bd5d9c4696
Matheus Henrique <matheus.181137@gmail.com>
2021-01-28 10:30:58 -0400

    add og image

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
-import { createGlobalStyle, ThemeProvider } from 'styled-components'
-import db from '../db.json'
+import { createGlobalStyle, ThemeProvider } from 'styled-components';
+import db from '../db.json';
+import Head from 'next/head';
 
 const GlobalStyle = createGlobalStyle`
   * {
@@ -30,6 +31,9 @@ const theme = db.theme
 export default function App({ Component, pageProps }) {
   return (
     <>
+    <Head>
+      <meta property="og:image" content={db.bg} key="image" />
+    </Head>
       <ThemeProvider theme={theme}>
         <GlobalStyle />
         <Component {...pageProps} />
