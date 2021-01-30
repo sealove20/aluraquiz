28a2ac9ec75c0a9e4fe44ee17ea1e5b328fd6616
Matheus Henrique <matheus.181137@gmail.com>
2021-01-30 11:56:13 -0400

    day 4

--- pages/index.js
+++ pages/index.js
@@ -1,45 +1,56 @@
+import React from 'react';
 import styled from 'styled-components';
+import { useRouter } from 'next/router';
+
 import db from '../db.json';
-import Widget from '../src/components/Widget'
-import Footer from '../src/components/Footer'
-import GitHubCorner from '../src/components/GitHubCorner'
-import QuizBackground from '../src/components/QuizBackground'
-
-const QuizContainer = styled.div`
-  width: 100%;
-  max-width: 350px;
-  padding-top: 45px;
-  margin: auto 10%;
-  @media screen and (max-width: 500px) {
-    margin: auto;
-    padding: 15px;
-  }
-`;
-
-
+import Widget from '../src/components/Widget';
+import Footer from '../src/components/Footer';
+import GitHubCorner from '../src/components/GitHubCorner';
+import QuizContainer from '../src/components/QuizContainer';
+import QuizBackground from '../src/components/QuizBackground';
+import Input from '../src/components/Input';
+import Button from '../src/components/Button';
 
 export default function Home() {
+  const router = useRouter();
+  const [name, setName] = React.useState('');
+
   return (
     <QuizBackground backgroundImage={db.bg}>
       <QuizContainer>
         <Widget>
-            <Widget.Header>
-              <h1>The Legend of Zelda</h1>
-            </Widget.Header>
+          <Widget.Header>
+            <h1>The Legend of Zelda</h1>
+          </Widget.Header>
           <Widget.Content>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet ullam, perspiciatis pariatur esse accusamus unde asperiores reprehenderit iure voluptate aspernatur nemo neque, aliquid tempore deleniti laborum odio dolores labore obcaecati.</p>
+            <form onSubmit={function (e) {
+              e.preventDefault();
+              console.log('Fazendo ua submissão por meio do react');
+              router.push(`/quiz?name=${name}`);
+            }}
+            >
+              <Input
+                name="nomeDousuario"
+                onChange={(e) => setName(e.target.value)}
+                placeholder="Diz aí seu nome"
+                value={name}
+              />
+              <Button type="submit" disabled={name.length === 0}>
+                {`Jogar ${name}`}
+              </Button>
+            </form>
           </Widget.Content>
         </Widget>
         <Widget>
           <Widget.Content>
             <h1>Quiz da galera</h1>
 
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet ullam, perspiciatis pariatur esse accusamus unde asperiores reprehenderit iure voluptate aspernatur nemo neque, aliquid tempore deleniti laborum odio dolores labore obcaecati.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
           </Widget.Content>
         </Widget>
         <Footer />
       </QuizContainer>
       <GitHubCorner projectUrl="https://github.com/sealove20" />
     </QuizBackground>
-  )
+  );
 }
